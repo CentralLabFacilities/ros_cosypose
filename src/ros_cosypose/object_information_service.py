8a907ffba80923f103e2fb59480c9bc08ac0c7e0
Igor Kalevatykh <kalevatykh@gmail.com>
2020-12-08 14:43:59 +0100

    Switch from pyassimp to trimesh

--- src/ros_cosypose/object_information_service.py
+++ src/ros_cosypose/object_information_service.py
@@ -1,24 +1,16 @@
 """This module describes the ObjectRecognitionServer."""
 
+import traceback
+
 import numpy as np
 import rospy
+import trimesh
 from cosypose.datasets.datasets_cfg import make_urdf_dataset
 from geometry_msgs.msg import Point
 from object_recognition_msgs.msg import ObjectInformation
 from object_recognition_msgs.srv import (GetObjectInformation,
                                          GetObjectInformationResponse)
 from shape_msgs.msg import Mesh, MeshTriangle
-
-try:
-    # tested on pyassimp=4.1.3
-    from pyassimp import pyassimp
-    use_pyassimp = True
-except:
-    try:
-        import pyassimp
-        use_pyassimp = True
-    except:
-        use_pyassimp = False
 
 from .object_database import ObjectDatabase
 
@@ -35,7 +27,7 @@ class ObjectInformationService:
         self._meshes = {}
 
         self._service = rospy.Service(
-            '/get_object_information', GetObjectInformation, self.execute)       
+            '/get_object_information', GetObjectInformation, self.execute)
 
     def execute(self, req):
         """Action server callback.
@@ -58,7 +50,7 @@ class ObjectInformationService:
             dataset = self._datasets[db]
 
             if (db, key) not in self._meshes:
-                self._meshes[(db, key)] = _make_mesh(
+                self._meshes[(db, key)] = self.make_mesh(
                     dataset.get_mesh_path(key),
                     dataset.get_mesh_scale(key))
             mesh = self._meshes[(db, key)]
@@ -72,48 +64,20 @@ class ObjectInformationService:
             rospy.logdebug(traceback.format_exc())
             raise ex
 
+    @staticmethod
+    def make_mesh(path, scale):
+        """Load a mesh from disk and convert to a message.
 
-def _make_mesh(path, scale):
-    """[summary]
+        Arguments:
+            path {str} -- path to the mesh file on disk
+            scale {list} -- mesh scale x,y,z
 
-    Arguments:
-        path {str} -- path to the mesh file on disk
-        scale {list} -- mesh scale x,y,z
+        Returns:
+            Mesh -- mesh message
+        """
+        rospy.logdebug('Make mesh: %s, scale: %.3f,%.3f,%.3f',  path, *scale)
 
-    Raises:
-        RuntimeError: pyassimp errors
-
-    Returns:
-        Mesh -- mesh message
-    """
-    rospy.logdebug('Make mesh: %s, scale: %.3f,%.3f,%.3f',  path, *scale)
-
-    mesh = Mesh()
-
-    if not use_pyassimp:
-        raise RuntimeError('pyassimp is broken, cannot load meshes')
-
-    scene = pyassimp.load(path)
-    try:
-        if not scene.meshes:
-            raise RuntimeError('Unable to load mesh "{}"'.format(path))
-
-        for face in scene.meshes[0].faces:
-            triangle = MeshTriangle()
-            if isinstance(face, np.ndarray):
-                indices = face.tolist()
-            else:
-                indices = face.indices
-            if len(indices) == 3:
-                triangle.vertex_indices = list(indices)
-            mesh.triangles.append(triangle)
-        for vertex in scene.meshes[0].vertices:
-            point = Point()
-            point.x = vertex[0] * scale[0]
-            point.y = vertex[1] * scale[1]
-            point.z = vertex[2] * scale[2]
-            mesh.vertices.append(point)
-    finally:
-        pyassimp.release(scene)
-
-    return mesh
+        mesh = trimesh.load(path, force='mesh')
+        return Mesh(
+            vertices=[Point(*vertex) for vertex in mesh.vertices * scale],
+            triangles=[MeshTriangle(vertex_indices=indices) for indices in mesh.faces])
